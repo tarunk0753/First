9c1ca97fa3ab8340f0744b04173a06b3b7727bf7
CS180 <tarun.kumar@chipspirit.com>
2024-08-13 14:43:19 +0530

    Second commit

---
+++ UVM/my_transaction.svh
@@ -0,0 +1,12 @@
+class my_transaction
+`uvm_object_utils(my_transaction)
+
+rand bit cmd; ///variables
+
+
+
+function new (string name ="");
+super.new(name);
+endfunction:new
+
+endclass: my_transaction
